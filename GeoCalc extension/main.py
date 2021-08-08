37e6499013de342955490273cf0026f96f29da51
Anton Khlebko <branhellward@gmail.com>
2021-08-08 13:21:00 +1000

    Update main file, add 2nd func

--- GeoCalc extension/main.py
+++ GeoCalc extension/main.py
@@ -1,5 +1,7 @@
 import sys
 import openpyxl
+import regex
+import pandas as pd
 from window import *
 
 
@@ -25,6 +27,30 @@ def geo_xlsx_to_txt():
         output_file.write(coord_line)
     output_file.close()
 
+def xy_xlsx_to_txt():
+    global select_file, output_txt
+    book = openpyxl.open(select_file, read_only=True)
+    sheet = book.active
+
+    i = open(output_txt,'w')
+
+    for row in range (2,sheet.max_row+1):
+        coord_line = str(
+            sheet[row][0].value)+'; '+str(
+                sheet[row][1].value)+'; '+str(
+                    sheet[row][2].value)+';\n'
+
+        i.write(coord_line)
+    i.close()
+
+    open_file = open(output_txt).read()
+    regular_ka = r'\1.\2'
+    fine_text = regex.sub(r'(\d{0})+[.,]+(\d{0})',regular_ka ,open_file)
+
+    wr = open(output_txt,'w')
+    wr.write(fine_text)
+    wr.close()
+
 
 class gce(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -33,7 +59,8 @@ class gce(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         
         self.ui.pushButton_3.clicked.connect(self.file_selection)
-        self.ui.pushButton.clicked.connect(self.save_file)
+        self.ui.pushButton.clicked.connect(self.save_file_geo)
+        self.ui.pushButton_2.clicked.connect(self.save_file_xy)
     
     def file_selection(self):
         global select_file
@@ -47,7 +74,7 @@ class gce(QtWidgets.QMainWindow):
             self.ui.pushButton_2.setDisabled(False)
         return select_file
     
-    def save_file(self):
+    def save_file_geo(self):
         global output_txt
         output_txt = False
         output_txt = QtWidgets.QFileDialog.getSaveFileName(self, "Save file", "Your file", "*.txt")[0]
@@ -55,6 +82,18 @@ class gce(QtWidgets.QMainWindow):
             QtWidgets.QMessageBox.about(self, 'Warning', 'Need to select the name and path of the file!')
         else:
             geo_xlsx_to_txt()
+            self.finish_label()
+            self.ui.pushButton.setDisabled(True)
+            self.ui.pushButton_2.setDisabled(True)
+
+    def save_file_xy(self):
+        global output_txt
+        output_txt = False
+        output_txt = QtWidgets.QFileDialog.getSaveFileName(self, "Save file", "Your file", "*.txt")[0]
+        if not output_txt:
+            QtWidgets.QMessageBox.about(self, 'Warning', 'Need to select the name and path of the file!')
+        else:
+            xy_xlsx_to_txt()
             self.finish_label()
             self.ui.pushButton.setDisabled(True)
             self.ui.pushButton_2.setDisabled(True)
