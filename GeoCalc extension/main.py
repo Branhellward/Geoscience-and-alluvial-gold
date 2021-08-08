f3ae7faafda52fba77ea023d4c611db30da0dbc8
Anton Khlebko <branhellward@gmail.com>
2021-08-08 18:19:54 +1000

    Add last func in main file

--- GeoCalc extension/main.py
+++ GeoCalc extension/main.py
@@ -102,7 +102,7 @@ def xy_txt_to_xlsx():
     worksheet.write('C1', 'Y', write_format_hat)
 
     a = 0
-    data_shape = data.shape[0]
+    # data_shape = data.shape[0]
     data_shape_2 = data.shape[0] - 1
     bcolmn = 'B'
     b = 0
@@ -128,6 +128,114 @@ def xy_txt_to_xlsx():
 
     workbook.close()
 
+def geo_txt_to_xlsx():
+    global select_file_2, output_xlsx
+    open_file = open(select_file_2).read()
+    cleaner = r'\1.\2'
+    fine_text = regex.sub(r'(\d{0})+[.,]+(\d{0})',cleaner ,open_file)
+
+    wr = open(select_file_2,'w')
+    wr.write(fine_text)
+    wr.close()
+
+    data = pd.read_csv(select_file_2, encoding='cp1251', header=None, sep=r"\s+", names=None, )
+    data=data.replace({';': ''}, regex=True)
+
+    workbook = xlsxwriter.Workbook(output_xlsx)
+    worksheet = workbook.add_worksheet('Coordinates')
+
+    # worksheet.set_column('D:D', 12)
+    # worksheet.set_column('G:G', 12)
+
+    merge_format = workbook.add_format({
+            'bold': 0,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': 'white'})
+
+    write_format = workbook.add_format({
+            'bold': 0,
+            'border': 1,
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': 'white'})
+
+    write_format_colum = workbook.add_format({
+            'bold': 0,
+            'border': 1,
+            'num_format': "00",
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': 'white'})
+
+    write_format1 = workbook.add_format({
+            'bold': 0,
+            'border': 1,
+            'num_format': "00.00000",
+            'align': 'center',
+            'valign': 'vcenter',
+            'fg_color': 'white'})
+
+    worksheet.merge_range('A1:A2', '№№', merge_format)
+    worksheet.merge_range('B1:D1', 'North Latitude', merge_format)
+    worksheet.merge_range('E1:G1', 'East Longitude', merge_format)
+    worksheet.write('B2', 'Degrees', write_format)
+    worksheet.write('C2', 'Minutes', write_format)
+    worksheet.write('D2', 'Seconds', write_format)
+    worksheet.write('E2', 'Degrees', write_format)
+    worksheet.write('F2', 'Minutes', write_format)
+    worksheet.write('G2', 'Seconds', write_format)
+    worksheet.write('A1', '№№', write_format1)
+
+    a = 0
+    data_shape = data.shape[0]
+    bcolmn = 'B'
+    b = 0
+    ccolmn = 'C'
+    c = 0
+    dcolmn = 'D'
+    d = 0
+    ecolmn = 'E'
+    e = 0
+    fcolmn = 'F'
+    f = 0
+    gcolmn = 'G'
+    g = 0
+    yy = -1
+
+    while a < data_shape:
+        a += 1
+        b += 1
+        c += 1
+        d += 1
+        e += 1
+        f += 1
+        g += 1
+        yy += 1
+        vertical_2 = ('A' + str(a + 2))
+        bb = (bcolmn + str(b + 2))
+        cc = (ccolmn + str(c + 2))
+        dd = (dcolmn + str(d + 2))
+        ee = (ecolmn + str(e + 2))
+        ff = (fcolmn + str(f + 2))
+        gg = (gcolmn + str(g + 2))
+
+        worksheet.write(vertical_2, a, write_format)
+        
+        float_iat_2 = float(data.iat[yy, 2])
+        float_iat_3 = float(data.iat[yy, 3])
+        float_iat_5 = float(data.iat[yy, 5])
+        float_iat_6 = float(data.iat[yy, 6])
+
+        worksheet.write(bb, data.iat[yy, 1], write_format)
+        worksheet.write(cc, float_iat_2, write_format_colum)
+        worksheet.write(dd, float_iat_3, write_format1)
+        worksheet.write(ee, data.iat[yy, 4], write_format)
+        worksheet.write(ff, float_iat_5, write_format_colum)
+        worksheet.write(gg, float_iat_6, write_format1)
+
+    workbook.close()
 
 class gce(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -140,6 +248,7 @@ class gce(QtWidgets.QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.save_file_xy)
         self.ui.pushButton_4.clicked.connect(self.file_selection_2)
         self.ui.pushButton_10.clicked.connect(self.save_file_xlsx_xy)
+        self.ui.pushButton_9.clicked.connect(self.save_file_xlsx_geo)
     
     def file_selection(self):
         global select_file
@@ -164,6 +273,18 @@ class gce(QtWidgets.QMainWindow):
             self.ui.pushButton_9.setDisabled(False)
             self.ui.pushButton_10.setDisabled(False)
         return select_file_2
+
+    def save_file_xlsx_geo(self):
+        global output_xlsx
+        output_xlsx = False
+        output_xlsx = QtWidgets.QFileDialog.getSaveFileName(self, "Save file", "Your file", "*.xlsx")[0]
+        if not output_xlsx:
+            QtWidgets.QMessageBox.about(self, 'Warning', 'Need to select the name and path of the file!')
+        else:
+            geo_txt_to_xlsx()
+            self.finish_label_2()
+            self.ui.pushButton_9.setDisabled(True)
+            self.ui.pushButton_10.setDisabled(True)
 
     def save_file_xlsx_xy(self):
         global output_xlsx
