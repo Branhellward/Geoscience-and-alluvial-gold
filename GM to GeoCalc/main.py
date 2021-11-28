8ed8e3dcd6483209d32c0388c85f5ae85d6100a7
Branhellward <branhellward@gmail.com>
2021-11-28 17:32:01 +0300

    Add new func

--- GM to GeoCalc/main.py
+++ GM to GeoCalc/main.py
@@ -1,9 +1,21 @@
-counter = 1
-replacements = {'°':'', 'N':';', 'E':';', '\'':'', '\"':''}
+def for_dms():
+    counter = 1
+    replacements = {'°':'', ' N':';', ' E':';', '\'':'', '\"':''}
 
-with open('1.txt') as input_file, open('3.txt', 'w') as output_file:
-    for line in input_file:
-        for src, target in replacements.items():
-            line = line.replace(src, target)
-        output_file.write( '{}{} {}'.format(counter,';', line))
-        counter += 1   
+    with open('1.txt') as input_file, open('3.txt', 'w') as output_file:
+        for line in input_file:
+            for src, target in replacements.items():
+                line = line.replace(src, target)
+            output_file.write( '{}{} {}'.format(counter,';', line))
+            counter += 1 
+
+def for_xy():
+    counter = 1
+    replacements = {'\n':';\n','\t':';\t'}
+
+    with open('4.txt') as input_file, open('5.txt', 'w') as output_file:
+        for line in input_file:
+            for src, target in replacements.items():
+                line = line.replace(src, target)
+            output_file.write( '{}{} {}'.format(counter,';', line))
+            counter += 1
