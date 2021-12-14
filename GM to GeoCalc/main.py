d8f4c729e3047082f57b568e744c8af1c8c05b01
Anton Khlebko <branhellward@gmail.com>
2021-12-14 21:58:08 +1000

    Add gui class to main file, add window.py

--- GM to GeoCalc/main.py
+++ GM to GeoCalc/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from window import *
+
 
 def for_dms():
     counter = 1
@@ -30,3 +32,40 @@ def for_xy():
     data = pd.read_csv('5.txt', encoding='cp1251', header=None, sep=r"\s+", names=None)
     data = data[[0,2,1]]
     data.to_csv('3.txt', header=None, index=None, sep=' ', mode='w')
+
+
+class gm_to_geocalc(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_gm_to_geocalc()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton_3.clicked.connect(self.instruction)
+        
+    def instruction(self):
+            QtWidgets.QMessageBox.about(self, 'Instruction', ''' 
+        Gm to GeoCalc
+    
+    1. Select the area you need
+    2. Export object:
+        File - Export - Export Vector/Lidar Format..
+    3. Select Export Format = Text File
+    4. Apply the settings:
+        Coordinate Separator: (TAB)
+        Feature Separator: (NONE)
+        Coordinate Order/Format: X
+        Coordinate Precision: Use Default Precision Based on Units
+        Others don't need to select
+    5. OK
+    6. Select a text file with coordinates
+    7. Click the button "Convert to DME or XY"
+    8. Select the name and path of the file (.txt)
+    9. Click "Save file"
+        ''')
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = gm_to_geocalc()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ GM to GeoCalc/window.py
@@ -0,0 +1,56 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+
+class Ui_gm_to_geocalc(object):
+    def setupUi(self, gm_to_geocalc):
+        gm_to_geocalc.setObjectName("gm_to_geocalc")
+        gm_to_geocalc.resize(440, 200)
+        gm_to_geocalc.setMinimumSize(QtCore.QSize(440, 200))
+        gm_to_geocalc.setMaximumSize(QtCore.QSize(440, 200))
+        self.pushButton = QtWidgets.QPushButton(gm_to_geocalc)
+        self.pushButton.setGeometry(QtCore.QRect(30, 65, 158, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(gm_to_geocalc)
+        self.label.setGeometry(QtCore.QRect(0, 10, 441, 41))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(gm_to_geocalc)
+        self.label_2.setGeometry(QtCore.QRect(30, 130, 158, 28))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(14)
+        self.label_2.setFont(font)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayoutWidget = QtWidgets.QWidget(gm_to_geocalc)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(250, 60, 160, 121))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
+        self.pushButton_4 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.verticalLayout.addWidget(self.pushButton_4)
+        self.pushButton_3 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout.addWidget(self.pushButton_3)
+
+        self.retranslateUi(gm_to_geocalc)
+        QtCore.QMetaObject.connectSlotsByName(gm_to_geocalc)
+
+    def retranslateUi(self, gm_to_geocalc):
+        _translate = QtCore.QCoreApplication.translate
+        gm_to_geocalc.setWindowTitle(_translate("gm_to_geocalc", "GM to GeoCalc"))
+        self.pushButton.setText(_translate("gm_to_geocalc", "Select file"))
+        self.label.setText(_translate("gm_to_geocalc", "Path"))
+        self.label_2.setText(_translate("gm_to_geocalc", "GM to GeoCalc"))
+        self.pushButton_2.setText(_translate("gm_to_geocalc", "Convert to DMS"))
+        self.pushButton_4.setText(_translate("gm_to_geocalc", "Convert to XY"))
+        self.pushButton_3.setText(_translate("gm_to_geocalc", "Instruction"))
